f397645efcf8185270358ff86978a10e1885c4e4
wynsley <wynsleyjhacksonduransalazar@gmail.com>
2025-08-12 09:00:46 -0500

    ADD: se corrigió los estilos del nav

--- src/components/pages/admissionPage.jsx
+++ src/components/pages/admissionPage.jsx
@@ -1,10 +1,5 @@
-import { Link } from "react-router-dom"
+
 import './admisionPage.css'
-import { GuestTemplate } from "../templates/guestTemplate"
-import CarouselNextButton from "../atoms/carouselNextButton"
-import CarouselPrevButton from "../atoms/carouselPrevButton"
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa6"
-import { AsideCarousel } from "../organisms/logicaCarousel"
 import { AdmisionSectionOne } from "../organisms/admisionSectionOne"
 import { InfraestructuraIPC } from "../organisms/admisionSectionIPC"
 import { AdmissionRequirements } from "../organisms/admisionRequirements"
@@ -12,43 +7,15 @@ import { SectionForm } from "../organisms/admissionForm"
 import { AdmisionSifts } from "../organisms/AdmisionSifts"
 
 
-import Img1 from "../../assets/img/infraestructura.png"
-import Img2 from "../../assets/img/seervicios4.jpg"
-import Img3 from "../../assets/img/servivios2.jpg"
-import { ImOpt } from "react-icons/im"
 
-const images = [
-    { src: Img1, alt: "Infraestructura" },
-    { src: Img2, alt: "Marcha" },
-    { src: Img3, alt: "Calculo" },
-]
+
 
 
 function AdmissionPage(){
   return (
         <>
-          <section className="section__carousel">
-            <AsideCarousel images={ images }>
-              {{
-                prev: ( goToPrev ) => (
-                  <CarouselPrevButton
-                    onClick={goToPrev}
-                    className="carousel__aside__btn  carousel__aside__btn--prev"
-                    >
-                      <FaChevronLeft />
-                    </CarouselPrevButton>
-                ),
-
-                next: ( goToNext ) => (
-                  <CarouselNextButton
-                    onClick={goToNext}
-                    className= "carousel__aside__btn carousel__aside__btn--next"
-                    >
-                      <FaChevronRight />
-                    </CarouselNextButton>
-                )
-              }}
-            </AsideCarousel>
+        <section className="section__carousel">
+           
         </section>
         <AdmisionSectionOne/>
         <InfraestructuraIPC/>
