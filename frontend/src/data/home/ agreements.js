f6410ac26c34147ce89832f189189da9335f9e26
wynsley <wynsleyjhacksonduransalazar@gmail.com>
2026-09-24 19:01:17 -0500

    feat(agreements): agregar nuevo archivo de datos para el componente Agreements con información educativa

---
+++ frontend/src/data/home/ agreements.js
@@ -0,0 +1,41 @@
+import {
+  PiBuildings,
+  PiStudent,
+  PiChalkboardTeacher,
+  PiUsersThree,
+  PiMonitorPlay,
+  PiLaptop
+} from "react-icons/pi"
+
+export const agreements = [
+  {
+    title: "UPN",
+    desc: "Universidad Privada del Norte — formación profesional de calidad.",
+    icon: PiBuildings ,
+  },
+  {
+    title: "Coronel Cortegana",
+    desc: "Institución educativa con historia y compromiso académico.",
+    icon: PiStudent ,
+  },
+  {
+    title: "Pedro Paula",
+    desc: "Centro educativo enfocado en el desarrollo integral del estudiante.",
+    icon: PiChalkboardTeacher ,
+  },
+  {
+    title: "AMM",
+    desc: "Asociación comprometida con la excelencia y la innovación educativa.",
+    icon: PiUsersThree ,
+  },
+  {
+    title: "IDAT",
+    desc: "Instituto de diseño y tecnología con enfoque aplicado y práctico.",
+    icon: PiMonitorPlay ,
+  },
+  {
+    title: "Cibertec",
+    desc: "Instituto tecnológico líder en carreras técnicas y digitales.",
+    icon: PiLaptop ,
+  },
+]
