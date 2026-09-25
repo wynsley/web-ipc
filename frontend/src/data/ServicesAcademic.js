af55e49c45119652a7c2a242d3c394e93a3ff7bc
wynsley <wynsleyjhacksonduransalazar@gmail.com>
2026-09-25 08:51:10 -0500

    feat(services): agregar datos de servicios académicos

---
+++ frontend/src/data/ServicesAcademic.js
@@ -0,0 +1,14 @@
+export const services = [
+  {
+    text: " Inscripción",
+    value: 100
+  },
+  {
+    text: "Matrícula",
+    value: 100
+  },
+  {
+    text: "Mensualidad",
+    value: 200
+  }
+]
