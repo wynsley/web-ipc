4ce99824198c55863c067b70c1ba4af7dd116263
wynsley <wynsleyjhacksonduransalazar@gmail.com>
2026-09-25 00:11:53 -0500

    feat(modal): agregar hooks useClickOutside y useModal para manejo de modales

---
+++ frontend/src/hooks/modal/usClickOutside.js
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from "react";
+
+export function useClickOutside (callback) {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (ref.current && !ref.current.contains(e.target)) {
+        callback();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [callback]);
+
+  return ref
+}
---
+++ frontend/src/hooks/modal/useModal.js
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+export const useModal = (initialValue = false) => {
+  const [isOpen, setIsOpen] = useState(initialValue)
+
+  const openModal = () => setIsOpen(true)
+  const closeModal = () => setIsOpen(false)
+
+  const toggleModal = () => {
+    isOpen ? closeModal : openModal 
+  }
+
+  return {
+    isOpen,
+    openModal,
+    closeModal,
+    toggleModal
+  }
+}
