fd013a9cda932d94fec0948d37bd17c6eb8e15f7
wynsley <wynsleyjhacksonduransalazar@gmail.com>
2026-09-26 01:15:08 -0500

    feat(HomeMessage): integrar componente Toast para mostrar mensajes de notificación

--- frontend/src/components/organisms/home/HomeMessage.jsx
+++ frontend/src/components/organisms/home/HomeMessage.jsx
@@ -1,8 +1,9 @@
 import { Button } from "../../atoms/button";
 import { ContactSteps } from "../../molecules/shared/contactSteps";
-import { FormField } from "../../molecules/shared/formItem";
+import { FormField } from "../../molecules/shared/formField";
 import { CONTACT_FORM_GROUPS } from "../../../data/contactFormFields";
 import { useContactForm } from "../../../hooks/globals/useContactForm";
+import { Toast } from "../../molecules/shared/toast";
 
 function HomeMessage() {
   const {
@@ -10,6 +11,8 @@ function HomeMessage() {
     activeStep,
     errorStep,
     fieldErrors,
+    toast,
+    closeToast,
     submitting,
     setValue,
     handleSubmit,
@@ -17,12 +20,18 @@ function HomeMessage() {
 
   return (
     <section
-      className="
+      className=" relative
         my-10 md:my-20 p-4 grid grid-cols-1 md:grid-cols-[9em_1fr] lg:grid-cols-[12em_1fr] gap-5
         mx-auto w-[96%] md:w-[90%] md:max-w-7xl py-6
         bg-blue-deep
       "
-    >
+    > 
+      <Toast
+        type={toast.type}
+        message={toast.message}
+        visible={toast.visible}
+        onClose={closeToast}
+      />
       <ContactSteps activeStep={activeStep} errorStep={errorStep} orientation="responsive" />
 
       <form
